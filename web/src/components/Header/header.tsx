aee9fc04ba503f5003dd973bd48178613ff622fd
Rafael Jesus <rafael.moura22@fatec.sp.gov.br>
2021-07-29 23:21:09 -0300

    componente Header finalizado para web

--- web/src/components/Header/header.tsx
+++ web/src/components/Header/header.tsx
@@ -1,39 +1,55 @@
 import React from 'react';
+
+import { PrimaryButton } from '../PrimaryButton/PrimaryButton';
 import './styles.css';
 
-interface headerProps {
+import LogoFatec from '../../assets/logo.svg';
 
-}
 
-export const Header = (props: headerProps) => {
+export const Header = () => {
   return (
     <header className="header-container">
-      <div className="header-content">
-        <div className="home-icon"></div>
+      <div className="header-content grid">
+        <div className="home-icon">
+          <img src={LogoFatec} alt="Fatec Santana de Parnaíba" />
+        </div>
 
         <nav>
           <ul>
             <li>
-              <a href="#">Sobre</a>
+              <a className='link' href="/">
+                <p>Sobre</p>
+              </a>
             </li>
             <li>
-              <a href="#">Cursos</a>
+              <a className='link' href="/">
+                <p>Cursos</p>
+              </a>
             </li>
             <li>
-              <a href="#">Departamentos</a>
+              <a className='link' href="/">
+                <p>Departamentos</p>
+              </a>
             </li>
             <li>
-              <a href="#">Noticias</a>
+              <a className='link' href="/">
+                <p>Noticias</p>
+              </a>
             </li>
             <li>
-              <a href="#">Aluno</a>
+              <a className='link' href="/">
+                <p>Aluno</p>
+              </a>
             </li>
             <li>
-              <a href="#">Professor</a>
+              <a className='link' href="/">
+                <p>Professor</p>
+              </a>
             </li>
-            <li>
-              <a href="#">Quero ser Fatec</a>
-            </li>
+            <PrimaryButton 
+              to={'/'}
+              title={'Quero ser Fatec'}
+            />
           </ul>
         </nav>
       </div>
