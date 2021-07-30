1d3699ec5bc5e20e1c88adc3d1f1974af785fd13
Rafael Jesus <rafael.moura22@fatec.sp.gov.br>
2021-07-29 23:22:42 -0300

    Adicionando lib react-router-dom

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+
 import { Header } from './components/Header/header';
 
 export default function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+      </Router>
     </div>
   );
 }
