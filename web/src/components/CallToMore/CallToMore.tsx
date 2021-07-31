08fd7b01c8fd44b1c58b53a234002593cf8f3d9d
Rafael Jesus <rafael.moura22@fatec.sp.gov.br>
2021-07-31 11:28:51 -0300

    criado componente CallToMore

---
+++ web/src/components/CallToMore/CallToMore.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BiChevronRight } from "react-icons/bi";
+import "./styles.css";
+import "../../global.css";
+
+interface CallToMoreProps {
+  title: string;
+  link: string;
+}
+
+export const CallToMore = (props: CallToMoreProps) => {
+  const { title, link } = props;
+  return (
+    <div className="call-to-more">
+      <div>
+        <p>{title}</p>
+      </div>
+      <div style={{marginTop: 6}}>
+        <BiChevronRight size="2rem"/>
+      </div>
+    </div>
+  );
+};
