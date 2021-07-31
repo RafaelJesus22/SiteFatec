3ad3f91258b9b2a3858d46a3c04302fe4d675f0f
Rafael Jesus <rafael.moura22@fatec.sp.gov.br>
2021-07-31 13:16:14 -0300

    Adicionados icones

---
+++ web/src/assets/icons/icons.tsx
@@ -0,0 +1,13 @@
+import Book from './book.svg';
+import Laptop from './laptop.svg';
+import Logo from './logo.svg';
+import People from './people.svg';
+import Work from './work.svg';
+
+export {
+  Book,
+  Laptop,
+  Logo,
+  People,
+  Work,   
+};
