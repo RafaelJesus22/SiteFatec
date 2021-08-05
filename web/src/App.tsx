09e91f1d781e34996eb389e1af733ba2edae1015
Rafael Jesus <rafael.moura22@fatec.sp.gov.br>
2021-08-05 20:23:24 -0300

    ...

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { CallToMore } from "./components/CallToMore/CallToMore";
+import { SectionTitle } from "./components/SectionTitle/SectionTitle";
 
-import { Header } from './components/Header/header';
+import { Header } from "./components/Header/header";
 
 export default function App() {
   return (
     <div className="App">
       <Router>
         <Header />
+        <div style={{ marginTop: 200 }}>
+          <SectionTitle
+            title={"A faculdade"}
+            subtitle={"porque estudar com a gente"}
+          />
+        </div>
       </Router>
     </div>
   );
