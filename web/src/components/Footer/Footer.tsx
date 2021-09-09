6bb9bdd7a7ea1def4911293b14b5d54f5c5430c7
Rafael Jesus <rafael.moura22@fatec.sp.gov.br>
2021-09-09 10:17:42 -0300

    Iniciando componente footer

---
+++ web/src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import React from 'react';
+import './styles.css';
+
+export const Footer = (): JSX.Element => {
+	return (
+		<div className="footer">
+			
+		</div>
+	);
+};
