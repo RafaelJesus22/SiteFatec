f0bc2113c0d7f4f628bfa20ae4d98f1e71b0e9d8
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2021-12-22 09:46:30 -0300

    ajustes no componente ProfessorCard

--- src/pages/Professores/Professores.tsx
+++ src/pages/Professores/Professores.tsx
@@ -8,7 +8,7 @@ import { Container } from "../../components/Container/Container";
 
 export const Professores: React.FC = () => {
   const [search, setSearch] = useState("");
-  const [prosessor, setProfessor] = useState<ProfessorCardProps>();
+  // const [prosessor, setProfessor] = useState<ProfessorCardProps>();
   const [, setCurso] = useState("");
   const CursoDisciplinas = [
     {
@@ -33,101 +33,101 @@ export const Professores: React.FC = () => {
   const professores: ProfessorCardProps[] = [
     {
       imgUrl: "https://i.pravatar.cc/150?img=1",
-      name: "Professor 01",
+      name: "Rafael Jesus Correia Moura",
       CursoDisciplinas,
     },
     {
-      imgUrl: "https://i.pravatar.cc/140?img=1",
-      name: "Professor 02",
+      imgUrl: "https://i.pravatar.cc/140?img=2",
+      name: "Professor 02Professor 02Professor 02",
       CursoDisciplinas,
     },
     {
-      imgUrl: "https://i.pravatar.cc/130?img=1",
+      imgUrl: "https://i.pravatar.cc/130?img=3",
       name: "Professor 03",
       CursoDisciplinas,
     },
     {
-      imgUrl: "https://i.pravatar.cc/155?img=1",
+      imgUrl: "https://i.pravatar.cc/155?img=4",
       name: "Professor 04",
       CursoDisciplinas,
     },
     {
-      imgUrl: "https://i.pravatar.cc/150?img=1",
-      name: "Professor 01",
+      imgUrl: "https://i.pravatar.cc/150?img=5",
+      name: "Pós doutor Irapuã Glória Junior",
       CursoDisciplinas,
     },
     {
-      imgUrl: "https://i.pravatar.cc/140?img=1",
+      imgUrl: "https://i.pravatar.cc/140?img=6",
       name: "Professor 02",
       CursoDisciplinas,
     },
     {
-      imgUrl: "https://i.pravatar.cc/130?img=1",
+      imgUrl: "https://i.pravatar.cc/130?img=7",
       name: "Professor 03",
       CursoDisciplinas,
     },
     {
-      imgUrl: "https://i.pravatar.cc/155?img=1",
+      imgUrl: "https://i.pravatar.cc/155?img=8",
       name: "Professor 04",
       CursoDisciplinas,
     },
     {
-      imgUrl: "https://i.pravatar.cc/150?img=1",
+      imgUrl: "https://i.pravatar.cc/150?img=9",
       name: "Professor 01",
       CursoDisciplinas,
     },
     {
-      imgUrl: "https://i.pravatar.cc/140?img=1",
+      imgUrl: "https://i.pravatar.cc/140?img=10",
       name: "Professor 02",
       CursoDisciplinas,
     },
     {
-      imgUrl: "https://i.pravatar.cc/130?img=1",
+      imgUrl: "https://i.pravatar.cc/130?img=11",
       name: "Professor 11",
       CursoDisciplinas,
     },
     {
-      imgUrl: "https://i.pravatar.cc/155?img=1",
+      imgUrl: "https://i.pravatar.cc/155?img=12",
       name: "Professor 12",
       CursoDisciplinas,
     },
     {
-      imgUrl: "https://i.pravatar.cc/150?img=1",
+      imgUrl: "https://i.pravatar.cc/150?img=13",
       name: "Professor 13",
       CursoDisciplinas,
     },
     {
-      imgUrl: "https://i.pravatar.cc/140?img=1",
+      imgUrl: "https://i.pravatar.cc/140?img=14",
       name: "Professor 14",
       CursoDisciplinas,
     },
     {
-      imgUrl: "https://i.pravatar.cc/130?img=1",
+      imgUrl: "https://i.pravatar.cc/130?img=15",
       name: "Professor 15",
       CursoDisciplinas,
     },
     {
-      imgUrl: "https://i.pravatar.cc/155?img=1",
+      imgUrl: "https://i.pravatar.cc/155?img=16",
       name: "Professor 16",
       CursoDisciplinas,
     },
     {
-      imgUrl: "https://i.pravatar.cc/150?img=1",
+      imgUrl: "https://i.pravatar.cc/150?img=17",
       name: "Professor 17",
       CursoDisciplinas,
     },
     {
-      imgUrl: "https://i.pravatar.cc/140?img=1",
+      imgUrl: "https://i.pravatar.cc/140?img=18",
       name: "Professor 18",
       CursoDisciplinas,
     },
     {
-      imgUrl: "https://i.pravatar.cc/130?img=1",
+      imgUrl: "https://i.pravatar.cc/130?img=19",
       name: "Professor 19",
       CursoDisciplinas,
     },
     {
-      imgUrl: "https://i.pravatar.cc/155?img=1",
+      imgUrl: "https://i.pravatar.cc/155?img=20",
       name: "Professor 20",
       CursoDisciplinas,
     },
