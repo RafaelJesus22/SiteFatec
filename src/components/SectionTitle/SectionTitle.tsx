5766165b4df732d7f635baa69f30bae2703b44a9
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2021-12-22 09:20:50 -0300

    ajuste no componente SectionTitle

--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -11,8 +11,14 @@ export const SectionTitle = (props: SectionTitleProps): JSX.Element => {
 
   return (
     <div className="section-title">
-      {subtitle && <h2 className="subtitle">{subtitle}</h2>}
-      <h1 className="title">{title}</h1>
+      {subtitle && (
+        <div className="subtitle-container">
+          <h2 className="subtitle">{subtitle}</h2>
+        </div>
+      )}
+      <div className="title-container">
+        <h1 className="title">{title}</h1>
+      </div>
     </div>
   );
 };
