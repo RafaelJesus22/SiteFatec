ef9c51a85de4f6ae52c91a6ae93cf6921f73d88d
Matheus Lima <rlmatheus79@gmail.com>
2022-03-21 19:47:17 -0300

    Criação do esqueleto do portal

--- src/pages/Portal/Login/index.tsx
+++ src/pages/Portal/Login/index.tsx
@@ -1,9 +1,22 @@
+import Logo from "../../../assets/icons/logo.svg";
+import imgPortal from "../../../assets/icons/imgPortal.svg";
 import './styles.css';
 
 export const PortalLogin = () => {
   return (
     <div className='portal'>
-      <h1>Portal Login</h1>
+      <div className='portal_header'>
+        <img src={Logo} alt='logo da Fatec' />
+      </div>
+      <div className='portal_content'>
+        <img src={imgPortal} alt='logo da Fatec' />
+        <h1 className='portal_title'>Bem - vindo</h1>
+        <p className='portal_description'>Este é o portal de administração do site da fatec.
+          Por favor, faça o login para entrar.
+        </p>
+        <input className='portal_inputEmail' type="email" placeholder="Email" />
+        <input className='portal_inputEmail' type="password" placeholder="Senha" />
+      </div>
     </div>
   );
 };
