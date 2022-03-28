f8aae387068e5afd7e10d4393c1bcb9b0e756031
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-03-28 10:10:10 -0300

    refatorando componente SearchInput

--- src/components/atoms/SearchInput/index.tsx
+++ src/components/atoms/SearchInput/index.tsx
@@ -1,5 +1,6 @@
-import { PortalInput } from "../PortalInput";
 import { FiSearch } from "react-icons/fi";
+
+import './styles.css';
 
 interface Props {
   value: string;
@@ -15,12 +16,13 @@ export const SearchInput: React.FC<Props> = ({
   return (
 
     <div className="search-input">
-      <PortalInput
-        style={style}
+      <FiSearch size={18} style={{ maxWidth: 24 }} color="415462" />
+      <input
+        type="text"
         value={value}
-        onChangeText={onChange}
+        onChange={(e) => onChange(e.target.value)}
+        style={style}
         placeholder="Pesquisar"
-        icon={() => <FiSearch size={18} style={{ maxWidth: 24 }} color="415462" />}
       />
     </div>
   );
