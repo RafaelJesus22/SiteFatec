c6c47f38794ed44d41bccc25860a47287bee4301
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-04-16 15:36:02 -0300

    adicionando imagem de loading

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -2,10 +2,12 @@ import ads from './ads.svg';
 import datascience from './data_science.svg';
 import gcom from './gcom.svg';
 import seginfo from './seg_info.svg';
+import loading from './loading.svg';
 
 export {
   ads,
   datascience,
   gcom,
   seginfo,
+  loading,
 };
