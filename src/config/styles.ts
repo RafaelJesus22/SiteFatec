c65bcca73fbe010bbc2c399bfa04673d738e4839
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-04-16 20:21:40 -0300

    adicionando variavel para o fundo dos modais no global.css e themas dos botões no styles.ts

--- src/config/styles.ts
+++ src/config/styles.ts
@@ -4,5 +4,18 @@ export const colors = {
   secondaryTextColor: '#757575',
   white: '#fff',
   black: '#000000',
-  background: '#fff'
+  background: '#fff',
+  modalBackground: '#0004',
+}
+
+export const buttonTheme = {
+  primary: {
+    backgroundColor: colors.primaryColor,
+    color: colors.white,
+  },
+  secondary: {
+    backgroundColor: colors.white,
+    color: colors.primaryColor,
+    border: `1px solid ${colors.primaryColor}`,
+  },
 }
