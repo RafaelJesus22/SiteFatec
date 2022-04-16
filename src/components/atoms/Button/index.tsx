70bbdf4a953f26baa44c815ac14fb6e3562275cf
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-04-16 20:22:13 -0300

    aceitando propriedade type no componente button

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,16 +1,25 @@
+import { buttonTheme } from '../../../config/styles';
 import './styles.css';
+
+type ButtonTheme = 'primary' | 'secondary';
 
 interface Props {
   title: string;
   onClick: () => void;
+  type?: ButtonTheme;
 }
 
 export const Button: React.FC<Props> = ({
   title,
   onClick,
+  type = 'primary',
 }) => {
   return (
-    <button className="app-button" onClick={onClick}>
+    <button
+      className="app-button" 
+      onClick={onClick}
+      style={buttonTheme[type]}
+    >
       {title}
     </button>
   );
