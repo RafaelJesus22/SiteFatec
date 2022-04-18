18d254487f1a124697fcb74f81b620910c893cb4
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-04-18 10:05:37 -0300

    adicionando serviço para as diciplinas

---
+++ src/services/subjectService.ts
@@ -0,0 +1,63 @@
+import { collection, getDocs, addDoc, updateDoc, doc, deleteDoc } from "firebase/firestore";
+import { CollectionsEnum } from "../enums/collections";
+import { db } from "../firebase";
+import { DbSubject } from "../types/ICurso";
+
+
+export class SubjectService {
+  private db = db;
+  private subjectsCollectionRef = collection(this.db, CollectionsEnum.disciplines);
+
+  public subjects: DbSubject[] = [];
+
+  private async getFirestoreSubjects(): Promise<DbSubject[]> {
+    const subjectsSnapshot = await getDocs(this.subjectsCollectionRef);
+
+    const subjects = subjectsSnapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    });
+
+    return subjects as DbSubject[];
+  }
+
+  public async getSubjects(storaged?: boolean): Promise<DbSubject[]> {
+    // lógica para não buscar os professores no 
+    // firebase toda vez que a página for carregada
+    if (this.subjects.length > 0 && !storaged) {
+      return this.subjects;
+    }
+
+    const subjects = await this.getFirestoreSubjects();
+
+    this.subjects = subjects as DbSubject[];
+    return this.subjects;
+  }
+
+  public async getOneSubject(id: string): Promise<DbSubject | undefined> {
+    const subjects = await this.getSubjects();
+    return subjects.find(subject => subject.id === id);
+  }
+
+  async createSubject(subject: DbSubject): Promise<void> {
+    const response = await addDoc(this.subjectsCollectionRef, subject);
+    console.log('criei', response);
+  }
+
+  async updateSubject(subject: DbSubject): Promise<void> {
+    if (subject && subject.id) {
+      const docRef = doc(db, CollectionsEnum.disciplines, subject.id);
+      const response = await updateDoc(docRef, {...subject});
+
+      console.log('atualizei', response);
+    }
+  }
+
+  async deleteSubject(id: string): Promise<void> {
+    const docRef = doc(db, CollectionsEnum.disciplines, id)
+    await deleteDoc(docRef);
+    console.log('deletei');
+  }
+}
