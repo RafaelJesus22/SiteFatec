c85ffc2becc60be0012f2e82127b53ac77e2f99d
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-05-06 15:57:11 -0300

    refactor: continuando refatoração da tela de professores Fazendo os filtros funcionarem

--- src/pages/Professores/Professores.tsx
+++ src/pages/Professores/Professores.tsx
@@ -1,13 +1,17 @@
-import React, { useLayoutEffect, useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useCallback, useEffect, useLayoutEffect, useState } from "react";
 import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
-import { Content } from "../../components/containers/Content/Content";
+
 import { Search } from "../../assets/icons/icons";
+import { DbProffessor } from "../../types/IProfessor";
+import { colors } from "../../config/styles";
+import { cursosService, proffessorsService } from "../../services";
+import { useLoading } from "../../contexts/loadingContent";
+
+import { Content } from "../../components/containers/Content/Content";
 import { Container } from "../../components/containers/Container/Container";
 import { Accordion } from "../../components/atoms/Accordion";
-import { DbProffessor } from "../../types/IProfessor";
-import { proffessorsService } from "../../services";
 import "./styles.css";
-import { colors } from "../../config/styles";
 
 interface ProffessorHeaderProps {
   proffessor: DbProffessor;
@@ -15,27 +19,117 @@ interface ProffessorHeaderProps {
   onPress: () => void;
 }
 
+interface SelectOptionProps {
+  label: string;
+  value: string;
+}
+
 export const Professores: React.FC = () => {
   const [search, setSearch] = useState("");
-  const [, setCurso] = useState("");
+  const [cursos, setCursos] = useState<SelectOptionProps[]>([{
+    label: 'Todos os cursos',
+    value: 'All'
+  }]);
+  const [selectedCourse, setSelectecCourse] = useState<SelectOptionProps>(cursos[0]);
   const [professores, setProfessores] = useState<DbProffessor[]>([]);
+  const [filteredProffessors, setFilteredProffessors] = useState<DbProffessor[]>([]);
   const [selectedProfessor, setSelectedProfessor] = useState<DbProffessor | undefined>();
+  const { hideLoading, showLoading } = useLoading();
 
   const fetchProffessors = async () => {
+    showLoading();
     const proffessors = await proffessorsService.getProffessors(true);
     setProfessores(proffessors);
+    setFilteredProffessors(proffessors);
+    hideLoading();
   };
 
+  const fetchCourses = useCallback(async () => {
+    showLoading();
+    const courses = await cursosService.getCourses(true);
+    setCursos([
+      {
+        label: 'Todos os cursos',
+        value: 'All'
+      },
+      ...courses.map(course => {
+        return {
+          label: course.name,
+          value: course.id
+        }
+      })
+    ]);
+
+    hideLoading();
+  }, []);
+
   const handleSelectProffessor = (proffessor: DbProffessor) => {
-    console.log('proffessor', proffessor.id);
     if (proffessor.id === selectedProfessor?.id) {
       return setSelectedProfessor(undefined);
     }
     return setSelectedProfessor(proffessor);
-  }
+  };
+
+  const fetchData = () => {
+    Promise.all([
+      fetchProffessors(),
+      fetchCourses(),
+    ]);
+  };
+
+  const filter = () => {
+    if (!search && selectedCourse.value === 'All') {
+      return setFilteredProffessors(professores);
+    }
+
+    if (!!search && selectedCourse.value === 'All') {
+      return setFilteredProffessors(
+        professores.filter(
+          professor =>
+            professor.name.toLowerCase().includes(search.toLowerCase())
+        )
+      );
+    }
+
+    const filtered: DbProffessor[] = [];
+
+    if (!search && selectedCourse.value !== 'All') {
+      professores.forEach(professor => {
+        const { classes } = professor;
+        classes.forEach(item => {
+          if (item.value === selectedCourse.value) {
+            filtered.push(professor);
+          }
+        })
+      });
+
+      return setFilteredProffessors(filtered);
+    }
+
+    professores.forEach(professor => {
+      const { classes } = professor;
+      classes.forEach(item => {
+        if (item.value === selectedCourse.value) {
+          filtered.push(professor);
+        }
+      })
+    });
+
+    return setFilteredProffessors(
+      filtered.filter(professor => professor.name.toLowerCase().includes(search.toLowerCase()))
+    );
+  };
+
+  useEffect(() => {
+    console.log('filtrados', filteredProffessors)
+  }, [filteredProffessors]);
+
+  useEffect(() => {
+    filter();
+  }, [search, selectedCourse]);
 
   useLayoutEffect(() => {
-    fetchProffessors();
+    fetchData();
   }, [])
 
   return (
@@ -46,13 +140,17 @@ export const Professores: React.FC = () => {
             <select
               name="curso"
               id="cursos"
-              onChange={(e): void => setCurso(e.target.value)}
+              onChange={(e): void =>
+                setSelectecCourse(
+                  cursos.filter(curso => curso.value === e.target.value)[0]
+                )
+              }
             >
-              <option value="">Todos os cursos</option>
-              <option value="ADS">Analise e desenvolvimento de sistemas</option>
-              <option value="CD">Ciência de dados</option>
-              <option value="SI">Segurança da informação</option>
-              <option value="GCOM">Gestão comercial</option>
+              {cursos.map(course => (
+                <option key={course.value} value={course.value}>
+                  {course.label}
+                </option>
+              ))}
             </select>
           </div>
           <div className="filtro__item">
@@ -67,14 +165,19 @@ export const Professores: React.FC = () => {
             />
           </div>
         </div>
+
         <div className="professores__list">
-          {professores.map((proffessor) => (
+          {filteredProffessors.length === 0 && (
+            <h2>Nenhum professor foi encontrado!</h2>
+          )}
+          {filteredProffessors.map((proffessor, index) => (
             <Accordion
               key={proffessor.id}
-              isOpen={selectedProfessor?.id === proffessor.id}
-              onClick={(): void => handleSelectProffessor(proffessor)}
+              zIndex={index + 1}
+              isOpen={!!selectedProfessor && (selectedProfessor?.id === proffessor.id)}
+              onClick={() => handleSelectProffessor(proffessor)}
               Content={() => <ProffessorContent {...proffessor} />}
-              Header={() => 
+              Header={() =>
                 <ProffessorHeader
                   proffessor={proffessor}
                   isOpen={selectedProfessor?.id === proffessor.id}
@@ -94,7 +197,7 @@ const ProffessorHeader: React.FC<ProffessorHeaderProps> = ({
   isOpen,
   onPress,
 }) => {
-  const {imgUrl, name, title} = proffessor;
+  const { imgUrl, name, title } = proffessor;
 
   return (
     <div className="proffessor-header" onClick={onPress}>
