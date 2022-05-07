2efa19802498333022233f5cfcc66ab24cc4bed7
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-05-07 13:23:21 -0300

    Feat: Ajustes na tela Home Ajustes na tela Cursos

--- src/pages/Cursos/Cursos.tsx
+++ src/pages/Cursos/Cursos.tsx
@@ -1,35 +1,16 @@
 import React from 'react';
 import { Container } from '../../components/containers/Container/Container';
 import { Content } from '../../components/containers/Content/Content';
-import { ads, datascience, gcom, seginfo } from '../../assets/images';
-import { CursoCardProps } from '../../components/molecules/CursoCard/CursoCard';
-
-import { CursosEnum } from '../../enums/cursos';
-import { CursoCard } from '../../components/molecules/CursoCard/CursoCard';
-
+import { Courses } from '../../components/organisms/Cursos';
 
 import './styles.css';
 
-const LOREN_50 = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla consequatur sequi neque ut magnam perferendis vitae sed architecto fugiat soluta.'
 export const Cursos: React.FC = () => {
-  const data: CursoCardProps[] = [
-    { title: 'Análise e Desenvolvimento de Sistemas', image: ads, description: LOREN_50, link: CursosEnum.ADS },
-    { title: 'Gestão Comercial', image: gcom, description: LOREN_50, link: CursosEnum.GCOM },
-    { title: 'Ciência de Dados', image: datascience, description: LOREN_50, link: CursosEnum.CIENCIA_DE_DADOS },
-    { title: 'Segurança da Informação', image: seginfo, description: LOREN_50, link: CursosEnum.SEGINFO },
-  ];
+  
   return (
     <Container>
-      <Content title={'Cursos'} isOnTop>
-      <div className="cursos-container">
-        {data.map((item, index) => {
-          return (
-            <div className="cursos-item" key={index}>
-              <CursoCard {...item}/>
-            </div>
-          );
-        })}
-      </div>
+      <Content title="Cursos" isOnTop>
+        <Courses />
       </Content>
     </Container>
   );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,3 @@
-import { CallToMore } from '../../components/atoms/CallToMore/CallToMore';
 import { Container } from '../../components/containers/Container/Container';
 import { Content } from '../../components/containers/Content/Content';
 import { LatestEvents } from '../../components/molecules/LatestEvents';
@@ -9,19 +8,21 @@ import { Courses } from '../../components/organisms/Cursos';
 export const Home = (): JSX.Element => {
   return (
     <Container>
-      <Content isOnTop title='A Faculdade' subtitle='Porque estudar com a gente' >
+      <Content
+        isOnTop
+        bottomSpace
+        title='A Faculdade'
+        subtitle='Porque estudar com a gente'
+      >
         <WhyUs />
       </Content>
-      <Content title="Nossos cursos">
-        <Courses />
+      <Content bottomSpace title="Nossos cursos">
+        <Courses quantity={4} inverted />
       </Content>
-      <Content title="Ultimos eventos">
+      <Content bottomSpace title="Ultimos eventos">
         <LatestEvents />
-        <div className="cta" style={{ marginBottom: '3rem' }}>
-          <CallToMore link={'/noticias'} title={'Todos os Eventos'} />
-        </div>
       </Content>
-      <Content title="Parceiros">
+      <Content bottomSpace title="Parceiros">
         <Partners />
       </Content>
     </Container>
