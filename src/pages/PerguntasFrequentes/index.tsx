632d88f55ba2b99519ca4b89ee84ba8c7ffade7d
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-05-08 11:39:32 -0300

    feat: ajustes na tela perguntas frequentes

--- src/pages/PerguntasFrequentes/index.tsx
+++ src/pages/PerguntasFrequentes/index.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai'
 import { Container } from "../../components/containers/Container/Container";
 import { Content } from "../../components/containers/Content/Content";
 import { Accordion } from "../../components/atoms/Accordion";
 import { colors } from "../../config/styles";
 import './styles.css';
+import { Link } from "react-router-dom";
 
 interface HeaderProps {
   isOpen: boolean;
@@ -12,36 +13,66 @@ interface HeaderProps {
   onClick?: () => void;
 }
 
+const LINK_VESTIBULAR_FATEC = "https://www.vestibularfatec.com.br/home/";
+
 export const PerguntasFrequentes: React.FC = () => {
   const [selectedQuestion, setSelectedQuestion] = useState<any>();
   const [questions] = useState([
     {
       id: 1,
       pergunta: "Como faço para me inscrever no vestibular?",
-      resposta: `Os vestibulares da Fatec são realizados em dois periodos do ano, um para ingressar no primeiro semestre (fevereiro)  e outro para o segundo semestre (agosto) do ano.
-      As incrições para o primeiro semestre geralmente ocorrem em entre Novembro e Dezembro. Para o segundo semestre entre Junho e Agosto.`
+      resposta: (
+        <Fragment>
+          <p>
+            Os vestibulares da Fatec são realizados em dois periodos do ano, 
+            um para ingressar no primeiro semestre (fevereiro)  
+            e outro para o segundo semestre (agosto) do ano.
+          </p>
+          <p>
+            As incrições para o primeiro semestre geralmente ocorrem em entre 
+            Novembro e Dezembro. Para o segundo semestre entre Junho e Agosto.
+          </p>
+          <p>
+            Para se inscrever,{' '}
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={LINK_VESTIBULAR_FATEC}
+              className="fq-link"
+            >
+              Clique aqui
+            </a>
+          </p>
+        </Fragment>
+      )
     },
     {
       id: 2,
-      pergunta: "Como faço para me inscrever no vestibular?",
+      pergunta: "O que é Tecnólogo?",
       resposta: `Os vestibulares da Fatec são realizados em dois periodos do ano, um para ingressar no primeiro semestre (fevereiro)  e outro para o segundo semestre (agosto) do ano.
       As incrições para o primeiro semestre geralmente ocorrem em entre Novembro e Dezembro. Para o segundo semestre entre Junho e Agosto.`
     },
     {
       id: 3,
-      pergunta: "Como faço para me inscrever no vestibular?",
+      pergunta: "Posso fazer pós graduação, mestrado, doutorado como tecnólogo?",
       resposta: `Os vestibulares da Fatec são realizados em dois periodos do ano, um para ingressar no primeiro semestre (fevereiro)  e outro para o segundo semestre (agosto) do ano.
       As incrições para o primeiro semestre geralmente ocorrem em entre Novembro e Dezembro. Para o segundo semestre entre Junho e Agosto.`
     },
     {
       id: 4,
-      pergunta: "Como faço para me inscrever no vestibular?",
+      pergunta: "Como é a prova do vestibular?",
       resposta: `Os vestibulares da Fatec são realizados em dois periodos do ano, um para ingressar no primeiro semestre (fevereiro)  e outro para o segundo semestre (agosto) do ano.
       As incrições para o primeiro semestre geralmente ocorrem em entre Novembro e Dezembro. Para o segundo semestre entre Junho e Agosto.`
     },
     {
       id: 5,
-      pergunta: "Como faço para me inscrever no vestibular?",
+      pergunta: "Quem pode tentar uma vaga?",
+      resposta: `Os vestibulares da Fatec são realizados em dois periodos do ano, um para ingressar no primeiro semestre (fevereiro)  e outro para o segundo semestre (agosto) do ano.
+      As incrições para o primeiro semestre geralmente ocorrem em entre Novembro e Dezembro. Para o segundo semestre entre Junho e Agosto.`
+    },
+    {
+      id: 5,
+      pergunta: "Onde fica a Fatec?",
       resposta: `Os vestibulares da Fatec são realizados em dois periodos do ano, um para ingressar no primeiro semestre (fevereiro)  e outro para o segundo semestre (agosto) do ano.
       As incrições para o primeiro semestre geralmente ocorrem em entre Novembro e Dezembro. Para o segundo semestre entre Junho e Agosto.`
     },
@@ -70,7 +101,7 @@ export const PerguntasFrequentes: React.FC = () => {
               )}
               Content={() => (
                 <AccordionContent>
-                  <p>{question.resposta}</p>
+                  {question.resposta}
                 </AccordionContent>
               )}
             />
