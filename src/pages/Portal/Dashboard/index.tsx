900f7afa21fa73e361e550fe443aa97ffa70591d
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-06-16 14:35:08 -0300

    feat: adicionando página inicial do portal

--- src/pages/Portal/Dashboard/index.tsx
+++ src/pages/Portal/Dashboard/index.tsx
@@ -1,9 +1,21 @@
 import { DashboardContainer } from "../../../components/containers/DashboardContainer";
+import { DashboardHomeMenu } from "../../../components/molecules/DashboardHomeMenu";
+import './styles.css';
 
 export const PortalDashboard: React.FC = () => {
+
   return (
     <DashboardContainer hasPadding>
-      <h1>Dashboard Home</h1>
+      <div className="portal-dashboard-welcome">
+        <h1>Página inicial</h1>
+        <h3>Bem vindo</h3>
+        <span>
+          Esse é o portal de administração da Fatec, clique
+          em um dos botões abaixo para abrir as configurações
+        </span>
+      </div>
+
+      <DashboardHomeMenu />
     </DashboardContainer>
   );
 };
