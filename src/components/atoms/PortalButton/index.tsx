5923b346b5f793f43ca572940028b9b7163fee87
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-06-18 10:39:53 -0300

    chore: adicionando prop disabled para o componente FormButton

--- src/components/atoms/PortalButton/index.tsx
+++ src/components/atoms/PortalButton/index.tsx
@@ -5,16 +5,18 @@ interface Props {
   title: string;
   onClick: (param?: any) => any | Promise<any>;
   color?: string;
+  disabled?: boolean;
 }
 
 export const FormButton: React.FC<Props> = ({
   title,
   onClick,
   color,
+  disabled,
 }) => {
   return (
     <button
-      className="portal-button"
+      className={`portal-button ${disabled ? 'disabled' : ''}`}
       onClick={onClick}
       style={{
         backgroundColor: !color ? colors.primaryColor : color,
