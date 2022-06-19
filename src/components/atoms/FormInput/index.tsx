54d817fad012c9c05a76f7bb30dab2ea5ae01a0a
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-06-19 10:49:41 -0300

    feat(events): adicionando prop maxLength no componente FormTextArea

--- src/components/atoms/FormInput/index.tsx
+++ src/components/atoms/FormInput/index.tsx
@@ -8,6 +8,7 @@ interface InputProps extends InputHTMLAttributes<any> {
 
 interface TextAreaProps extends TextareaHTMLAttributes<any> {
   name: string;
+  maxLength?: number;
 }
 
 export const FormInput: React.FC<InputProps> = ({
@@ -27,12 +28,19 @@ export const FormInput: React.FC<InputProps> = ({
 
 export const FormTextArea: React.FC<TextAreaProps> = ({
   name,
+  maxLength,
+  value,
   ...props
 }) => {
   return (
     <div className="form-input">
       <label>{name}</label>
       <textarea {...props} className="form-input" />
+      {maxLength && (
+        <span className="max-lenght-label">
+          {value?.toString().length || 0}/{maxLength}
+        </span>
+      )}
     </div>
   );
 };
