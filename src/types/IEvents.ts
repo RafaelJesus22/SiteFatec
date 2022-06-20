ed33e0081d7782894fb21b80a9126362029f387a
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-06-19 21:30:37 -0300

    feat(events): adicionando na tipagem DbEvent os campos title createdAt edited updatedAt

--- src/types/IEvents.ts
+++ src/types/IEvents.ts
@@ -1,6 +1,10 @@
 export interface DbEvent {
   id: string;
+  title: string;
   post: string;
+  createdAt: Date;
+  edited: boolean;
+  updatedAt?: Date;
   imgURl?: string;
   likes?: number;
   courseParentId?: string;
