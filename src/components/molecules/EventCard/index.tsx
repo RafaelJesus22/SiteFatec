cfbf34684c776f13fd28bbfffb7e2d6de24d6832
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-06-20 11:29:54 -0300

    feat(events): adicionando componente para listagem de eventos no portal style(events): alterações no componente EventCard e PortalEventCard

--- src/components/molecules/EventCard/index.tsx
+++ src/components/molecules/EventCard/index.tsx
@@ -1,4 +1,5 @@
 import { useLayoutEffect, useState } from "react";
+import { FiEdit2, FiTrash2 } from "react-icons/fi";
 import { useHistory } from "react-router-dom";
 import { cursosService } from "../../../services";
 import { DbEvent } from "../../../types/IEvents"
@@ -6,6 +7,12 @@ import { DbEvent } from "../../../types/IEvents"
 import './styles.css';
 
 const MAX_WIDTH = 250;
+
+interface PortalEventCardProps {
+  event: DbEvent;
+  onPressEdit: (id: string) => void;
+  onPressDelete: (id: string) => void;
+}
 
 export const EventCard: React.FC<DbEvent> = ({
   createdAt,
@@ -70,7 +77,7 @@ export const EventCard: React.FC<DbEvent> = ({
             </span>
           )}
         </div>
-      </div> 
+      </div>
       <div className="event-item-body">
         <h3 onClick={handleEventDetails}>
           {title}
@@ -92,6 +99,90 @@ export const EventCard: React.FC<DbEvent> = ({
             alt="Imagem do post"
           />
         )}
+      </div>
+    </div>
+  );
+}
+
+export const PortalEventCard: React.FC<PortalEventCardProps> = ({
+  event,
+  onPressEdit,
+  onPressDelete,
+}) => {
+  const [courseName, setCourseName] = useState("");
+  const history = useHistory();
+
+  const getCourseName = async (courseParentId?: string) => {
+    if (!courseParentId) {
+      return setCourseName("Fatec Santana de Parnaíba");
+    }
+
+    const course = await cursosService.getCourseById(courseParentId);
+    return setCourseName(course?.name || "");
+  };
+
+  const handleDate = (date: any) => {
+    if (!date) {
+      return "";
+    }
+
+    const d = new Date(date.seconds * 1000);
+    const month = `0${d.getMonth() + 1}`.slice(-2);
+    return `${d.getDate()}/${month}/${d.getFullYear()}`;
+  };
+
+  const handlePost = (text: string) => {
+    if (!text) {
+      return "";
+    }
+    return text.length > MAX_WIDTH ? `${text.substring(0, MAX_WIDTH)}...` : text;
+  };
+
+  const handleEventDetails = () => {
+    history.push(`/portal/dashboard/evento/${event.id}`);
+  };
+
+  useLayoutEffect(() => {
+    getCourseName(event.courseParentId);
+  })
+
+  return (
+    <div className="event-item">
+      <div className="event-item-header">
+        <div>
+          <h3>{courseName}</h3>
+          <span>{handleDate(event.createdAt)}</span>
+        </div>
+        <div className="actions">
+          <FiEdit2
+            size={24}
+            className="icon"
+            onClick={() => onPressEdit(event.id)}
+          />
+          <FiTrash2
+            size={24}
+            className="icon delete"
+            onClick={() => {
+              onPressDelete(event.id);
+            }}
+          />
+        </div>
+      </div>
+      <div className="event-item-body">
+        <h3 onClick={handleEventDetails}>
+          {event.title}
+        </h3>
+        <p>
+          {handlePost(event.post)}
+          {event.post.length > MAX_WIDTH && (
+            <span
+              className="event-item-body_ver-mais"
+              onClick={handleEventDetails}
+            >
+              ver mais
+            </span>
+          )}
+        </p>
       </div>
     </div>
   );
