879d5392b62e3d83dd14693e37f4455a0c0bb4ce
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-06-19 22:30:55 -0300

    chore(app): adicionando função para lidar com o tipo Timestamp do firebase

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -1,3 +1,5 @@
+import { Timestamp } from "firebase/firestore";
+
 export const removeAccents = (str: string) => {
   return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 }
@@ -5,3 +7,12 @@ export const removeAccents = (str: string) => {
 export const transformToLink = (str: string) => {
   return removeAccents(str).toLowerCase().replace(/\s/g, '_');
 }
+
+export const firebaseTimestampToDate = (timestamp: any) => {
+  const date = new Date(timestamp?.seconds * 1000);
+
+  const months = `0${date.getMonth() + 1}`.slice(-2);
+  const day = `0${date.getDate()}`.slice(-2);
+
+  return `${day}/${months}/${date.getFullYear()}`;
+};
