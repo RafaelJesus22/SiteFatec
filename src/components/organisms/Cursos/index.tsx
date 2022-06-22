c1772c65831558203156f3ad8dd1dcbfa817dce3
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-06-22 15:22:51 -0300

    chore: utilizando container Feed nos cursos

--- src/components/organisms/Cursos/index.tsx
+++ src/components/organisms/Cursos/index.tsx
@@ -1,14 +1,15 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, Fragment } from 'react';
 import { cursosService } from '../../../services';
 import { DbCurso } from '../../../types/ICurso';
 import { CursoCard } from '../../molecules/CursoCard/CursoCard';
 import { CallToMore } from '../../atoms/CallToMore/CallToMore';
+import { Feed } from '../../containers/Feed';
 import './styles.css';
 
 interface Props {
   quantity?: number;
   inverted?: boolean;
-  containerStyle?: React.CSSProperties; 
+  containerStyle?: React.CSSProperties;
 }
 
 export const Courses: React.FC<Props> = ({
@@ -32,25 +33,27 @@ export const Courses: React.FC<Props> = ({
   }, [fetchCourses]);
 
   return (
-    <div className="cursos-container" style={containerStyle}>
-      {courses.map((item, index) => {
-        return (
-          <div className="cursos-item" key={item.id}>
-            <CursoCard
-              inverted={inverted && index % 2 === 1}
-              description={item.info.professionalProfile.slice(0, 150) + '..'}
-              image={item.imageUrl}
-              title={item.name}
-              link={item.link}
-            />
+    <Feed>
+      <Fragment>
+        {courses.map((item, index) => {
+          return (
+            <div className="cursos-item" key={item.id}>
+              <CursoCard
+                inverted={inverted && index % 2 === 1}
+                description={item.info.professionalProfile.slice(0, 150) + '..'}
+                image={item.imageUrl}
+                title={item.name}
+                link={item.link}
+              />
+            </div>
+          );
+        })}
+        {!!quantity && (
+          <div className="cta">
+            <CallToMore link={'/cursos'} title={'Todos os Cursos'} />
           </div>
-        );
-      })}
-      {!!quantity && (
-        <div className="cta">
-          <CallToMore link={'/cursos'} title={'Todos os Cursos'} />
-        </div>
-      )}
-    </div>
+        )}
+      </Fragment>
+    </Feed>
   );
 };
