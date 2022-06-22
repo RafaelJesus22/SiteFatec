33d5f04b6e097668e654c2515943f53841c9d6e2
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-06-22 14:43:24 -0300

    refactor(footer): adicionando link do canal do youtube style: alterando a cor dos ícones no hover chore: trocando ícones baixados pelos ícones da lib react-icons chore: adicionando link no email e whatssapp

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Facebook, LinkedIn } from '../../../assets/icons/icons';
+// import { Facebook, LinkedIn } from '../../../assets/icons/icons';
+import { FaYoutube, FaFacebookSquare } from 'react-icons/fa'
+import { BsLinkedin } from 'react-icons/bs'
 import './styles.css';
 
 const FATEC_LINKEDIN_URL = 'https://www.linkedin.com/in/fatec-santana-de-parna%C3%ADba-fatecsdp-5baa86198/';
 const FATEC_FACEBOOK_URL = 'https://www.facebook.com/fatecsdpoficial';
+const FATEC_YOUTUBE = 'https://www.youtube.com/channel/UCC9ZtMbAw--P4zyWY0YUgfQ';
+const FATEC_LINK_WHATSAPP = 'https://api.whatsapp.com/send?phone=5511993868094'
 
 const MATHEUS_LINKEDIN_URL = 'https://www.linkedin.com/in/matheuslimar/';
 const RAFAEL_LINKEDIN_URL = 'https://www.linkedin.com/in/rafaeljesuscm/';
@@ -26,18 +30,23 @@ export const Footer = (): JSX.Element => {
           <div className="info">
             <p className="title">CONTATO</p>
             <p>Tel: (11) 2424-2757</p>
-            <p>WhatsApp: (11) 99386-8094</p>
-            <p>E-mail: fatec.santana@fatec.sp.gov.br</p>
+            <p>WhatsApp: <a href={FATEC_LINK_WHATSAPP} target="_blank">(11) 99386-8094</a></p>
+            <p>E-mail: <a href="mailto:fatec.santana@fatec.sp.gov.br">fatec.santana@fatec.sp.gov.br</a></p>
           </div>
 
           <div className="social">
             <p>Siga nos:</p>
             <div className="icons">
               <a href={FATEC_FACEBOOK_URL} target="_blank" rel="noreferrer">
-                <img src={Facebook} alt="Facebook" />
+                {/* <img src={Facebook} alt="Facebook" /> */}
+                <FaFacebookSquare fill={'#fff'} size={'3rem'} className="facebook-icon" />
               </a>
               <a href={FATEC_LINKEDIN_URL} target="_blank" rel="noreferrer">
-                <img src={LinkedIn} alt="LinkedIn" />
+                {/* <img src={LinkedIn} alt="LinkedIn" /> */}
+                <BsLinkedin fill={'#fff'} size={'3rem'} className="linkedin-icon" />
+              </a>
+              <a href={FATEC_YOUTUBE} target="_blank" rel="noreferrer">
+                <FaYoutube fill={'#fff'} size={'3rem'} className="youtube-icon" />
               </a>
             </div>
           </div>
