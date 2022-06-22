729e85e08ed4c7b92d0b04cb4c5576d93f90528e
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-06-22 15:29:49 -0300

    reafactor: colocando conteúdo da página noticias dentro do container Feed

--- src/pages/Noticias/index.tsx
+++ src/pages/Noticias/index.tsx
@@ -1,11 +1,11 @@
 import React, { useCallback, useLayoutEffect, useState } from 'react';
 import { Container } from '../../components/containers/Container/Container';
 import { Content } from '../../components/containers/Content/Content';
+import { Feed } from '../../components/containers/Feed';
 import { EventCard } from '../../components/molecules/EventCard';
 import { useLoading } from '../../contexts/loadingContent';
 import { eventService } from '../../services';
 import { DbEvent } from '../../types/IEvents';
-import './styles.css';
 
 export interface NewsDetailParams {
   noticiaId: string;
@@ -37,16 +37,14 @@ export const Noticias: React.FC = () => {
   return (
     <Container>
       <Content title={'Notícias'} isOnTop >
-        <div className="news">
-          <div className="feed">
-            {events?.map(event => (
-              <EventCard
-                key={event.id}
-                {...event}
-              />
-            ))}
-          </div>
-        </div>
+        <Feed>
+          {events?.map(event => (
+            <EventCard
+              key={event.id}
+              {...event}
+            />
+          ))}
+        </Feed>
       </Content>
     </Container>
   );
