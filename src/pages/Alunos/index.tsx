6cde8f07b9d0d45f13dc1930665cab61e46ef5ec
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-06-26 15:13:08 -0300

    feat(students): colocando cpa na tela alunos

--- src/pages/Alunos/index.tsx
+++ src/pages/Alunos/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { LabelText } from '../../components/atoms/Typography/LabelText';
 import { Container } from '../../components/containers/Container/Container';
 import { Content } from '../../components/containers/Content/Content';
@@ -10,13 +10,72 @@ export const Alunos: React.FC = () => {
     <Container>
       <Content title={'Alunos'} isOnTop >
         <Feed>
-          <LabelText
-            bold
-            size='large'
-            spacing={{ marginBottom: '1rem', marginTop: '2rem' }}
-          >
-            Links
-          </LabelText>
+          <Fragment>
+            <LabelText
+              bold
+              size='large'
+              spacing={{ marginBottom: '1rem', marginTop: '2rem' }}
+            >
+              Links
+            </LabelText>
+          </Fragment>
+
+          <Fragment>
+            <LabelText
+              bold
+              size='large'
+              spacing={{ marginBottom: 12 }}
+            >
+              O QUE É CPA NA FATEC SANTANA DE PARNAÍBA?
+            </LabelText>
+            <LabelText
+              size="default"
+              color="secondary"
+              spacing={{ marginBottom: 12 }}
+            >
+              Denominada Comissão Própria de Avaliação - CPA, é um órgão colegiado, formado
+              por professores, que disciplina a organização, o funcionamento e o cumprimento das
+              atribuições e é vinculado ao Centro Estadual de Educação Tecnológica do estado de
+              São Paulo - CPS.
+            </LabelText>
+            <LabelText
+              size="default"
+              color="secondary"
+              spacing={{ marginBottom: 12 }}
+            >
+              A CPA tem atuação autônoma em relação aos Conselhos Superiores e demais Órgãos
+              Colegiados da Instituição.
+            </LabelText>
+            <LabelText
+              size="default"
+              color="secondary"
+              spacing={{ marginBottom: 12 }}
+            >
+              A CPA tem por finalidade contribuir com o planejamento, elaboração, coordenação e
+              monitoramento da política de autoavaliação institucional, promovendo, no que
+              couber, a interlocução com os órgãos de regulação, supervisão e avaliação.
+            </LabelText>
+            <LabelText
+              size="default"
+              color="secondary"
+              spacing={{ marginBottom: 12 }}
+            >
+              A autoavaliação tem por objetivo a melhoria da qualidade do ensino tecnológico, a
+              orientação da expansão da sua oferta, a consolidação da função social do ensino
+              superior e o desenvolvimento institucional, e consiste em um processo contínuo,
+              sistêmico e participativo que envolve comunidade acadêmica e integrantes da sociedade.
+            </LabelText>
+            <LabelText
+              size="default"
+              color="secondary"
+              spacing={{ marginBottom: 100 }}
+            >
+              Em nossa unidade, a CPA começou em 2020 com o Professor Antonio Lobosco, em 2021 foi
+              a professora Angela dos Santos Oshiro e em 2022 o Prof. Dr. Célio Aparecido Garcia.
+              Para qualquer dúvida, sugestão ou reclamação, você pode acionar a CPA pelo e-mail
+              f283.cpa@fatec.sp.gov.br
+            </LabelText>
+          </Fragment>
         </Feed>
       </Content>
     </Container>
