30c86e2b3be40e9efeff7c2700862b39529e1651
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-06-26 16:11:21 -0300

    refactor(events): colocando tela detalhes noticia dento de um container feed

--- src/pages/Noticias/DetalhesNoticial/index.tsx
+++ src/pages/Noticias/DetalhesNoticial/index.tsx
@@ -1,13 +1,15 @@
-import { Container } from "../../../components/containers/Container/Container";
-import { Content } from "../../../components/containers/Content/Content";
+import { useLayoutEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 import { NewsDetailParams } from '..';
-import { useParams } from "react-router-dom";
-import { DbEvent } from "../../../types/IEvents";
-import { useLayoutEffect, useState } from "react";
 import { useLoading } from "../../../contexts/loadingContent";
 import { cursosService, eventService } from "../../../services";
 import { firebaseTimestampToDate } from "../../../utils/strings";
+import { DbEvent } from "../../../types/IEvents";
+
+import { Container } from "../../../components/containers/Container/Container";
+import { Content } from "../../../components/containers/Content/Content";
+import { Feed } from "../../../components/containers/Feed";
 
 import './styles.css';
 
@@ -42,26 +44,28 @@ export const NewsDetail = () => {
         isOnTop
         title={courseName}
       >
-        <div className="detalhes-noticia">
-          {event?.imgURl && (
-            <img
-              src={event?.imgURl}
-              alt={event?.title}
-            />
-          )}
-          <div className="detalhes-noticia-text">
-            <h1>{event?.title}</h1>
-            <span>
-              criado em {firebaseTimestampToDate(event?.createdAt)}
-            </span>
-            {event?.edited && (
-              <span>
-                atualizado em {firebaseTimestampToDate(event?.updatedAt)}
-              </span>
+        <Feed>
+          <div className="detalhes-noticia">
+            {event?.imgURl && (
+              <img
+                src={event?.imgURl}
+                alt={event?.title}
+              />
             )}
-            <p>{event?.post}</p>
+            <div className="detalhes-noticia-text">
+              <h1>{event?.title}</h1>
+              <span>
+                criado em {firebaseTimestampToDate(event?.createdAt)}
+              </span>
+              {event?.edited && (
+                <span>
+                  atualizado em {firebaseTimestampToDate(event?.updatedAt)}
+                </span>
+              )}
+              <p>{event?.post}</p>
+            </div>
           </div>
-        </div>
+        </Feed>
       </Content>
     </Container>
   )
